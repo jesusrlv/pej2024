5c2eb5f406f0c60e8666944ad3205837a9e36114
jesusrlv <jesusrlvrojo@gmail.com>
2024-06-11 13:05:15 -0600

    cambio de fecha

--- prcd/verficacion.php
+++ prcd/verficacion.php
@@ -12,7 +12,7 @@ if (isset($_POST)) {
     if ($result->num_rows > 0) {
         $resticcion = $result->fetch_assoc();
         $varRest = $resticcion['categoria'];
-        if($varRest == 2022 || $varRest == 2021 || $varRest == 2020){
+        if($varRest == 2021 || $varRest == 2022 || $varRest == 2023){
             echo '<div class="alert alert-warning text-start"><strong><i class="bi bi-x-circle-fill"></i> ERROR.</strong> YA HAS SIDO GANADOR DEL PEJ EN EDICIONES ANTERIORES. A partir de la fecha en que ganaste, puedes participar nuevamente en 3 años.</div>
         
             <style>
